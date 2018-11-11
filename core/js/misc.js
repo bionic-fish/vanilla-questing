454e42ca9e096db7118fe61f664dd51dc671b225
wickstjo <33825194+wickstjo@users.noreply.github.com>
2018-11-11 19:48:24 +0200

    Fixed issues & added to db

--- core/js/misc.js
+++ core/js/misc.js
@@ -1,76 +1,41 @@
-function set_points(block) {
+function set_points(block, response) {
 
    // PURGE SELECTORS
    $('#map').html('');
    $('#tooltip').html('');
    $('#tooltip').css('display', 'none');
 
-   // WAIT FOR BOTH PROMISES TO RESOLVE
-   $.getJSON('../data/blocks.json').then((response) => {
+   // ATTACH CORRECT MAP
+   $('#map').css('background', 'url("interface/img/maps/' + response.blocks[block].zone + '.jpg")');
 
-      // ATTACH CORRECT MAP
-      $('#map').css('background', 'url("interface/img/maps/' + response[block].zone + '.jpg")');
+   // WAYPOINTS SHORTHAND
+   var waypoints = response.blocks[block].waypoints;
 
-      // WAYPOINTS SHORTHAND
-      var waypoints = response[block].waypoints;
-
-      // LOOP THROUGH EACH WAYPOINT OF THIS BLOCK
-      $.each(waypoints, (id, waypoint) => {
-         
-         // CONSTRUCT WAYPOINT
-         var wp = `
-            <div class="waypoint" style="left: ` + waypoint.coords.x + `%; top: ` + waypoint.coords.y + `%;">
-               <img src="interface/img/waypoints/space.png" class="` + waypoint.type + `" wp="` + id + `">
-            </div>
-         `;
-
-         // APPEND IT TO THE MAP
-         $('#map').append(wp);
-      });
-
-      // SHOW TOOLTIP ON MOUSEOVER
-      $('body').on('mouseover', 'img', (event) => {
-
-         // PICK UP DETAILS
-         var id = $(event.target).attr('wp');
-         var waypoint = waypoints[id];
-
-         // GENERATE WAYPOINT DATA
-         var header = '';
-         var ends = '';
-         var starts = '';
-         var objectives = '';
-
-         // GENERATE DIVS FOR FILLED PROPERTIES
-         waypoint.ends.forEach(quest_name => { ends += '<div class="ends">' + quest_name + '</div>'; });
-         waypoint.starts.forEach(quest_name => { starts += '<div class="starts">' + quest_name + '</div>'; });
-         waypoint.objectives.forEach(quest_name => { objectives += '<div class="objectives">' + quest_name + '</div>'; });
-         if (waypoint.header != '') { header += '<div class="header">' + waypoint.header + '</div>'; }
-
-         // RENDER IN WAYPOINT DATA
-         $('#tooltip').html(header + ends + starts + objectives);
-
-         // TOOLTIP PROPERTIES
-         var tooltip = {
-            width: parseFloat($('#tooltip').css('width')),
-            height: parseFloat($('#tooltip').css('height')),
-            padding: parseInt($('#tooltip').css('padding')[0]),
-            offset: 5
-         }
-
-         // FIGURE OUT THE XY POSITION WITH OFFSETS
-         var x = event.target.x - (tooltip.width / 2);
-         var y = event.target.y - (tooltip.height + (tooltip.offset + (2 * tooltip.padding)));
+   // LOOP THROUGH EACH WAYPOINT OF THIS BLOCK
+   $.each(waypoints, (id, waypoint) => {
       
-         // SET CSS
-         $('#tooltip').css('left', x);
-         $('#tooltip').css('top', y);
-         $('#tooltip').css('display', 'inline-block');
-      });
-      
-      // HIDE TOOLTIP ON MOUESOUT
-      $('body').on('mouseout', 'img', () => { $('#tooltip').css('display', 'none'); });
+      // CONSTRUCT WAYPOINT
+      var wp = `
+         <div class="waypoint" style="left: ` + waypoint.coords.x + `%; top: ` + waypoint.coords.y + `%;">
+            <img src="interface/img/waypoints/space.png" class="` + waypoint.type + `" wp="` + id + `">
+         </div>
+      `;
+
+      // APPEND IT TO THE MAP
+      $('#map').append(wp);
    });
+
+   // LINE CONTAINER
+   var lines = '';
+
+   // GENERATE LINES
+   for(var x = 0; x < waypoints.length - 1; x++) {
+      var line = '<line x1="' + waypoints[x].coords.x + '%" y1="' + waypoints[x].coords.y + '%" x2="' + waypoints[x + 1].coords.x + '%" y2="' + waypoints[x + 1].coords.y + '%"/>';
+      lines += line;
+   }
+
+   // APPEND AN SVG ONTOP OF THE BACKGROUND & DRAW LINES
+   $('#map').append('<svg>' + lines + '</svg>');
 }
 
 // SHORTHAND FOR CONSOLE LOGGING
