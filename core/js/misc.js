25f9bd7530256fc14a541681c338c6f20c29566e
wickstjo <33825194+wickstjo@users.noreply.github.com>
2018-11-12 08:03:21 +0200

    Added darkshore to db

--- core/js/misc.js
+++ core/js/misc.js
@@ -1,41 +1,48 @@
 function set_points(block, response) {
 
-   // PURGE SELECTORS
-   $('#map').html('');
-   $('#tooltip').html('');
-   $('#tooltip').css('display', 'none');
+   $('#map').css('opacity', 0);
 
-   // ATTACH CORRECT MAP
-   $('#map').css('background', 'url("interface/img/maps/' + response.blocks[block].zone + '.jpg")');
+   sleep(200).then(() => {
 
-   // WAYPOINTS SHORTHAND
-   var waypoints = response.blocks[block].waypoints;
+      // PURGE SELECTORS
+      $('#map').html('');
+      $('#tooltip').html('');
+      $('#tooltip').css('display', 'none');
 
-   // LOOP THROUGH EACH WAYPOINT OF THIS BLOCK
-   $.each(waypoints, (id, waypoint) => {
-      
-      // CONSTRUCT WAYPOINT
-      var wp = `
-         <div class="waypoint" style="left: ` + waypoint.coords.x + `%; top: ` + waypoint.coords.y + `%;">
-            <img src="interface/img/waypoints/space.png" class="` + waypoint.type + `" wp="` + id + `">
-         </div>
-      `;
+      // ATTACH CORRECT MAP
+      $('#map').css('background', 'url("interface/img/maps/' + response.blocks[block].zone + '.jpg")');
 
-      // APPEND IT TO THE MAP
-      $('#map').append(wp);
+      // WAYPOINTS SHORTHAND
+      var waypoints = response.blocks[block].waypoints;
+
+      // LOOP THROUGH EACH WAYPOINT OF THIS BLOCK
+      $.each(waypoints, (id, waypoint) => {
+         
+         // CONSTRUCT WAYPOINT
+         var wp = `
+            <div class="waypoint" style="left: ` + waypoint.coords.x + `%; top: ` + waypoint.coords.y + `%;">
+               <img src="interface/img/waypoints/space.png" class="` + waypoint.type + `" wp="` + id + `">
+            </div>
+         `;
+
+         // APPEND IT TO THE MAP
+         $('#map').append(wp);
+      });
+
+      // LINE CONTAINER
+      var lines = '';
+
+      // GENERATE LINES
+      for(var x = 0; x < waypoints.length - 1; x++) {
+         var line = '<line x1="' + waypoints[x].coords.x + '%" y1="' + waypoints[x].coords.y + '%" x2="' + waypoints[x + 1].coords.x + '%" y2="' + waypoints[x + 1].coords.y + '%"/>';
+         lines += line;
+      }
+
+      // APPEND AN SVG ONTOP OF THE BACKGROUND & DRAW LINES
+      $('#map').append('<svg>' + lines + '</svg>');
+
+      $('#map').css('opacity', 1);
    });
-
-   // LINE CONTAINER
-   var lines = '';
-
-   // GENERATE LINES
-   for(var x = 0; x < waypoints.length - 1; x++) {
-      var line = '<line x1="' + waypoints[x].coords.x + '%" y1="' + waypoints[x].coords.y + '%" x2="' + waypoints[x + 1].coords.x + '%" y2="' + waypoints[x + 1].coords.y + '%"/>';
-      lines += line;
-   }
-
-   // APPEND AN SVG ONTOP OF THE BACKGROUND & DRAW LINES
-   $('#map').append('<svg>' + lines + '</svg>');
 }
 
 // SHORTHAND FOR CONSOLE LOGGING
